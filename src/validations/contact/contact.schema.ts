437bdbe5722f89aea1b418d9c78f2a24bc7e09dd
Maxwell Laner <maxwellaner@gmail.com>
2021-12-18 09:52:15 -0300

    Add yup package and validation schema to contact form

---
+++ src/validations/contact/contact.schema.ts
@@ -0,0 +1,10 @@
+import * as Yup from 'yup';
+
+const create = Yup.object().shape({
+  name: Yup.string().required('Campo obrigatório'),
+  contactType: Yup.string().required('Campo obrigatório'),
+  email: Yup.string().email('Deve ser um e-mail válido').required('Campo obrigatório'),
+  phone: Yup.string().min(15, 'Este campo deve conter 11 dígitos').required('Campo obrigatório')
+});
+
+export default { create }
