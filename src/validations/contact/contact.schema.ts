4083db0e7b4f643c9077dc3b4dffa266f77e17cd
Maxwell Laner <maxwellaner@gmail.com>
2021-12-18 13:42:24 -0300

    Update schema

--- src/validations/contact/contact.schema.ts
+++ src/validations/contact/contact.schema.ts
@@ -1,7 +1,7 @@
 import * as Yup from 'yup';
 
 const create = Yup.object().shape({
-  name: Yup.string().required('Campo obrigatório'),
+  name: Yup.string().required('Campo obrigatório').matches(/^[aA-zZ\s]+$/, 'Apenas letras'),
   contactType: Yup.string().required('Campo obrigatório'),
   email: Yup.string().email('Deve ser um e-mail válido').required('Campo obrigatório'),
   phone: Yup.string().min(15, 'Este campo deve conter 11 dígitos').required('Campo obrigatório')
