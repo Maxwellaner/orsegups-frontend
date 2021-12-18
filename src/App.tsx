c8d52312307c94950affd4904765730686f4d3ef
Maxwell Laner <maxwellaner@gmail.com>
2021-12-18 09:50:36 -0300

    Add routes to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import styles from './App.module.css';
+import { ContactPage } from './components/contact/ContactPage';
+import { ContactList } from './components/contactList/ContactList';
+import { ContactForm } from './components/contactForm/ContactForm';
+import { Sidebar } from './components/sidebar/Sidebar';
 
 function App() {
   return (
-    <div className={styles.App}>
-      
-    </div>
+    <main className={styles.main}>
+      <BrowserRouter>
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<ContactForm />} />
+          <Route path="/contact/:id" element={<ContactPage />} />
+          <Route path="/contacts" element={<ContactList />} />
+          <Route path="/contact/edit" element={<ContactForm />} />
+        </Routes>
+      </BrowserRouter>
+    </main>
   );
 }
 
